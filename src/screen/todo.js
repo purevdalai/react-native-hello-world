626624e695ca750eb0cfe6414b81f2e597ba738a
Purevdalai <purewdalai3@gmail.com>
2019-05-23 12:00:59 +0800

    Added todo page content

--- src/screen/todo.js
+++ src/screen/todo.js
@@ -1,12 +1,61 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Container, Content, Form, Item, Input, Text, Label, Picker, Button } from 'native-base';
+import { Icon } from 'react-native-vector-icons';
 
 export default class TodoScreen extends React.Component {
-    render() {
-        return (
-            <View>
-                <Text>Todo Screen</Text>
-            </View>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected2: undefined
+    };
+  }
+  onValueChange2(value) {
+    this.setState({
+      selected2: value
+    });
+  }
+ 
+  render() {
+    return (
+      <Container>
+        <Content>
+          <Form>
+            
+          <Item picker last>
+              <Picker
+                mode="dropdown"
+                iosIcon={<Icon name="arrow-down"  />}
+                style={{ width: undefined }}
+                placeholder="Select your SIM"
+                placeholderStyle={{ color: "#bfc6ea" }}
+                placeholderIconColor="#007aff"
+                selectedValue={this.state.selected2}
+                onValueChange={this.onValueChange2.bind(this)}
+              >
+                <Picker.Item label="Wallet" value="key0" />
+                <Picker.Item label="ATM Card" value="key1" />
+                <Picker.Item label="Debit Card" value="key2" />
+                <Picker.Item label="Credit Card" value="key3" />
+                <Picker.Item label="Net Banking" value="key4" />
+              </Picker>
+            </Item>
+
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input secureTextEntry={true} />
+            </Item>
+
+            <Button full style={{marginTop: 20}}>
+              <Text>Primary</Text>
+            </Button>
+          </Form>
+        </Content>
+      </Container>
+    )
+  }
 }
